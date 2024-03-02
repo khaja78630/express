691ac5252f88775a4ebf9e4bded0993315c53549
khaja78630 <khaja78630@gmail.com>
2024-03-02 10:31:26 +0530

    post api changes

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const app = express();
-const cors= require('cors');
 
+const cors= require('cors');
+const bodyParser = require('body-parser');
+const fs= require('fs')
+
+app.use(bodyParser.json())
 app.use(cors());
 
-app.get('/users', (request, response) => {
 const users = [
     {id : 1, name : 'Vinay'},
     {id : 2, name : 'Sai'},
     {id : 3, name : 'Khaja'}
 ]
+
+app.post('/addUser', (request, response) => {
+    users.push(request.body);
+   
+    response.send(JSON.stringify(users))
+})
+
+app.get('/users', (request, response) => {
+
 response.send(JSON.stringify(users))
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    //DB call
+    if(req.body.userId === 123 && req.body.pasword === 'Test'){
+        res.send(JSON.stringify({isUserPresent : true})) 
+    } else {
+        res.send(JSON.stringify({isUserPresent : false})) 
+    }
+   
 })
 app.listen(3001, () => {
     console.log('server is running on port number 3000')
